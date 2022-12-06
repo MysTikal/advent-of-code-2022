ee9a1f36bb5285b9d9644287846a48e41fd7cb4b
MysTikal <mystikal.uk@gmail.com>
2022-12-06 17:56:17 +0000

    Simplified day 6

--- 6/aoc6.py
+++ 6/aoc6.py
@@ -1,18 +1,14 @@
 with open('input', 'r') as file:
     data = file.read()
 
-def uniqueList(list):
-    return len(set(list)) == len(list)
-    
 def parseChar(str):
     charList = []
     for i, char in enumerate(str):
         charList.append(char)
         if len(charList) == 15:
             charList.pop(0)
-        if len(charList) == 14:
-            if uniqueList(charList):
-                print(i+1)
-                break
+        if len(set(charList)) == 14:
+            print(i+1)
+            break
 
 parseChar(data)
